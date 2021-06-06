db6d4d5a8ffc125e08dd81222c15d9951ac3f3a3
hadar-g <hadar.goldstein@gmail.com>
2021-06-05 21:10:48 -0700

    removed cunstructor from square, made board responsible for game state

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button 
+        className="square" 
+        onClick ={() => this.props.onClick({value: 'X'})}
+        >
           {this.props.value}
         </button>
       );
@@ -14,8 +17,17 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
     renderSquare(i) {
-      return <Square value = {i}/>;
+      return <Square 
+      value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />;
     }
   
     render() {
