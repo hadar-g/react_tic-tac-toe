2150b9f3709c3949a590097f897276239d512655
hadar-g <hadar.goldstein@gmail.com>
2021-06-06 12:54:49 -0700

    added O's

--- src/index.js
+++ src/index.js
@@ -19,14 +19,19 @@ function Square (props) {
     constructor(props){
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext : true,
         }
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X': 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        
+        })
     }
 
     renderSquare(i) {
